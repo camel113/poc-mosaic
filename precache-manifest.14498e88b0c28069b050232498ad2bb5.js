9b301cead4f4d22771c50127264d92ac93d178ef
camel113 <adrienbigler@gmail.com>
2019-06-07 14:08:38 +0200

    Updates

--- precache-manifest.14498e88b0c28069b050232498ad2bb5.js
+++ precache-manifest.14498e88b0c28069b050232498ad2bb5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fec9d48e3ad4fa8292a09e885f667018",
+    "revision": "d3a6640d678507c08a394f9cb6f63013",
     "url": "/poc-mosaic/index.html"
   },
   {
-    "revision": "2732e0a58898e8101a40",
+    "revision": "716b2f058201b6032542",
     "url": "/poc-mosaic/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "6776bb44712b9ef37dc7",
-    "url": "/poc-mosaic/static/js/2.11db198c.chunk.js"
+    "revision": "7400aebfb69281a3346e",
+    "url": "/poc-mosaic/static/js/2.879b8901.chunk.js"
   },
   {
-    "revision": "2732e0a58898e8101a40",
-    "url": "/poc-mosaic/static/js/main.25c1f9eb.chunk.js"
+    "revision": "716b2f058201b6032542",
+    "url": "/poc-mosaic/static/js/main.e5558d93.chunk.js"
   },
   {
     "revision": "fd208728ef607718014e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poc-mosaic/precache-manifest.7d7c9025f5346d9636f159b549479886.js"
+  "/poc-mosaic/precache-manifest.14498e88b0c28069b050232498ad2bb5.js"
 );
 
 self.addEventListener('message', (event) => {

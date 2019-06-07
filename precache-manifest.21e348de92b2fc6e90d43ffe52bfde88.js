18f153e14a3957989bead7c465f983c8cb05fbe3
camel113 <adrienbigler@gmail.com>
2019-06-07 13:36:20 +0200

    Updates

---
+++ precache-manifest.21e348de92b2fc6e90d43ffe52bfde88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103bad5f5bcf8eb77b740af2bc7c327d",
+    "url": "/poc-mosaic/index.html"
+  },
+  {
+    "revision": "32e3594ad3c7315877c5",
+    "url": "/poc-mosaic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6776bb44712b9ef37dc7",
+    "url": "/poc-mosaic/static/js/2.11db198c.chunk.js"
+  },
+  {
+    "revision": "32e3594ad3c7315877c5",
+    "url": "/poc-mosaic/static/js/main.ce1b5cb8.chunk.js"
+  },
+  {
+    "revision": "fd208728ef607718014e",
+    "url": "/poc-mosaic/static/js/runtime~main.a3cc383c.js"
+  },
+  {
+    "revision": "a5a0322b5552f24b5db9d298bc43b51b",
+    "url": "/poc-mosaic/static/media/selfie.a5a0322b.jpg"
+  }
+]);
